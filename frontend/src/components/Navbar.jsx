b5f58d864753d7625e6c9600b93c8d8f6f2f2fb6
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-19 23:51:40 +0530

    new commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {assets} from '../assets/assests'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-4 font medium'>
+
+       <img src={assets.logo} className='w-36' alt=""/>
+
         </div>
   )
 }
