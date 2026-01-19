61dcc62716328a2ad4842fd52c6d280820b7b1ab
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-19 20:35:41 +0530

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Collection from './pages/Collection.jsx'
+import About from './pages/About.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
+      <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/collection' element={<Collection/>} />
+       <Route path='/about' element={<About/>} />
+       <Route path='/contact' element={<Contact/>} />
+       <Route path='/product/: productId' element={<Product/>} />
+       <Route path='/cart' element={<Cart/>} />
+       <Route path='login' element={<Login/>} />
+       <Route path='/placeorder' element={<PlaceOrder/>} />
+       <Route path='/order' element={<Orders/>} />
+      </Routes>
+    </div>
   )
 }
 
