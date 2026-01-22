b6454e404ae1c0c207d30ef7490367b8f7db22e7
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-23 01:02:37 +0530

    new commit

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/* Hero Left Section */}
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='text- #414141'>
+      <div className='flex items-center gap-2'>
+
+      </div>
+
+        </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -59,10 +59,10 @@ const Navbar = () => {
              <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
              <p>Back</p>
               </div>
-              <NavLink to='/'>HOME</NavLink>
-              <NavLink to='/collection'>COLLECTION</NavLink>
-              <NavLink to='/about'>ABOUT</NavLink>
-              <NavLink to='/contact'>CONTACT</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
            </div>
          </div>
         </div>
