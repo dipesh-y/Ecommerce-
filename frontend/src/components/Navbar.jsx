4122a3ebd98261b5585db9aa88bb91cf553cd266
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-23 00:08:02 +0530

    new commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -56,7 +56,8 @@ const Navbar = () => {
          <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0' }`}>
            <div className='flex flex-col text-gray-600'>
               <div className='flex items-center gap-4 p-3'>
-             <img src={assets.dropdown_icon} alt="" />
+             <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+             <p>Back</p>
               </div>
            </div>
          </div>
