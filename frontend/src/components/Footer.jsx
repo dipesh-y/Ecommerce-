a09bfebe0e9b545b64947b5dccb1a3faafbe9d78
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-24 01:48:32 +0530

    New commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,6 +10,15 @@ const Footer = () => {
         <p className='w-full m:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, asperiores velit? Soluta voluptas sed debitis aperiam at distinctio, nam aut animi, dolor odio, aliquid accusantium omnis a facilis consequatur aliquam?
         </p>
     </div>
+    <div>
+        <p>COMPANY</p>
+        <ul className='flex flex-col gap-1 text-gray-600'>
+         <li>Home</li>
+         <li>About</li>
+         <li>Delivery</li>
+         <li>Privacy Policy</li>
+        </ul>
+    </div>
         </div>
     </div>
   )
