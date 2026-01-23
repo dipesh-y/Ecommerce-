f0f8a0e9993a1313aac8e622ad7eb30663cb538c
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-23 23:14:27 +0530

    New commit

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext.jsx';
 
 const LatestCollection = () => {
+
+const {products} = useContext(ShopContext);
+
+console.log(products);
+
+
   return (
     <div>LatestCollection</div>
   )
