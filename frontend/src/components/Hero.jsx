e0db4f162bc8a057d25e0d04eda5d790d1a6ac17
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-23 21:45:37 +0530

    new commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -11,14 +11,13 @@ const Hero = () => {
           <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
           <p className='font-medium text-sm md:text-base'>OUR BESTSELLER</p>
       </div>
-        <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+        <h1 className='prata-regulartext-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
         <div className="flex items-center gap-2">
           <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
           <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
         </div>
-
         </div>
-    </div>
+      </div>
         {/* Hero Right Section */}
         <img className='w-full sm:w-1/2' src={assets.hero_img} alt=" "/>
         </div>
