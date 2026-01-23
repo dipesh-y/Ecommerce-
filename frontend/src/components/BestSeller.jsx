fa6e04bde8972ad86f03da5ca00636460f3e52de
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-24 00:47:34 +0530

    New commit

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext.jsx'
+import Title from './Title.jsx'
 
 const BestSeller = () => {
-const {products} = React.useContext(ShopContext);
+  const { products } = useContext(ShopContext)
+  const [bestSeller, setBestSeller] = useState([])
+
+  useEffect(() => {
+    if (products && products.length > 0) {
+      const bestProducts = products.filter(item => item.bestseller)
+      setBestSeller(bestProducts.slice(0, 5))
+    }
+  }, [products])
 
   return (
-    <div>BestSeller</div>
+    <div className='my-10'>
+      <div className="text-center text-3xl py-8">
+        <Title text1={'BEST'} text2={'SELLER'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit iure iusto excepturi doloribus inventore nisi autem, ad similique, est aut repellat placeat officiis enim dolore minima, aliquam nam fugiat dicta.
+        </p>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LatestCollection from '../components/LatestCollection.jsx'
 import Hero from '../components/Hero.jsx'
+import BestSeller from '../components/BestSeller.jsx'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   )
 }
