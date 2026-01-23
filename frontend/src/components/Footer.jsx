711d40a3d0bb57804c1631c23410d096213e3c45
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-24 01:40:43 +0530

    new commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+    <div>
+        <img src={assets.logo} className='mb-5 w-32' alt=""/>
+        <p className='w-full m:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, asperiores velit? Soluta voluptas sed debitis aperiam at distinctio, nam aut animi, dolor odio, aliquid accusantium omnis a facilis consequatur aliquam?
+        </p>
+    </div>
+        </div>
+    </div>
   )
 }
 
