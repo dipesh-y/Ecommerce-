5403f244891986308cd4c112afc9c0b11b349655
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-23 23:41:35 +0530

    new commit

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -5,11 +5,15 @@ const LatestCollection = () => {
 
 const {products} = useContext(ShopContext);
 
-console.log(products);
-
-
   return (
-    <div>LatestCollection</div>
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTION'} />
+            <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quis alias nesciunt voluptatem ullam voluptatum natus, totam voluptate quibusdam aspernatur quidem beatae, incidunt quo nihil, ab ducimus debitis possimus ea.
+            </p>
+        </div>
+    </div>
   )
 }
 
