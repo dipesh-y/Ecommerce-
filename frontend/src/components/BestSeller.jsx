861a8ec16979bb51bef0712419596c7217a26a22
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-24 00:58:05 +0530

    new commit

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -23,13 +23,17 @@ const BestSeller = () => {
         </p>
       </div>
 
-     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4" >
-
-        {
-            bestSeller.map((item, index) => (
-                <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price} />))
-        }
-
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {bestSeller.map((item, index) => (
+          <ProductItem 
+            key={index} 
+            id={item.id} 
+            image={item.image} 
+            name={item.name} 
+            price={item.price} 
+          />
+        ))}
+      </div>
     </div>
   )
 }
