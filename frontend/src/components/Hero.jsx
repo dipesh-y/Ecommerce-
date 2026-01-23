4ba477fdd859c7ecbdaae46e1ef2d1c141ee642d
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-23 20:27:03 +0530

    new commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -20,6 +20,7 @@ const Hero = () => {
         </div>
     </div>
         {/* Hero Right Section */}
+        <img className='w-full sm:w-1/2' src={assets.hero_img} alt=" "/>
         </div>
   )
 }
