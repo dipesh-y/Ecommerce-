cbf0ffe15dc29a907718a5f9f83ce41e77337edf
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-25 19:35:27 +0530

    New Commit

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 
 const Collection = () => {
 const { products } = useContext(ShopContext);
+const [showFilters, setShowFilters] = React.useState(false);
 
   return (
     <div classname="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -11,6 +12,10 @@ const { products } = useContext(ShopContext);
     <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>
       FILTERS
     </p>
+     {/* Category Filter*/}
+      <div className= {` border border-gray-300 pl-5 py-3 mt-6`}>
+
+      </div>
     </div>
 
     </div>
