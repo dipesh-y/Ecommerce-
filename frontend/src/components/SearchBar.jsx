20d903ccf7e2c486cee95ecb4c5e6e51315603ad
Nausheen Siddiqui <nausheenzmr@gmail.com>
2026-01-26 00:53:58 +0530

    new commit

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,8 +4,14 @@ const SearchBar = () => {
  
 const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
 
-  return (
-    <div>
+  return showSearch ? (
+    <div className='border-t border-b bg-gray-50 text-center'>
+        <div className='inline-flex items-center justify-center border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 '>
+            <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm'
+              type="text" 
+              placeholder='Search'    />
+            <img className='w-4' src={assets.search_icon} alt="search" />
+        </div>
 
     </div>
   )
