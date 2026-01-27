2bc27ae3656f9c297e6881e4c3f24dbfe9917502
Ariga Charani <159914046+Acharani@users.noreply.github.com>
2026-01-28 00:52:42 +0530

    till related product completed

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,11 +9,39 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10; // Flat delivery fee
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems,setCartItems]= useState({});
+
+
+    const addToCart = async (ListItemSecondaryAction,size) =>{
+
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]) {
+            if(cartData[itemId][size]){
+               cartData[itemId][size] +=1;
+            }
+            else{
+                cartData[itemId][size]=1;
+            }
+        }
+        else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+        }
+        setCartItems(cartData);
+
+    
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
     
     const value = { 
         products, currency, delivery_fee, 
-        search, setSearch, showSearch, setShowSearch
-    };
+        search, setSearch, showSearch, setShowSearch,
+        cartItems,addToCart
+    }
     return(
         <ShopContext.Provider value={value}>
             {props.children}
