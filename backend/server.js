120fe55e53ea87369aff85892a0cf29fc9dd3ae1
Aman Chaudhary <aman.chaudhary01257@gmail.com>
2026-02-08 17:13:17 +0530

    completed till 08:00:00

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
+import connectDB from './conflg/mongodb.js'
+import connectCloudinary from './conflg/cloudinary.js'
+import userRouter from './route/userRoute.js'
+import productRouter from './route/productRoute.js'
 
 //App Config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
-app.get('/',(req,res)=>{
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+
+app.get('/', (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port, ()=>console.log('Server started on PORT : '+ port))
+app.listen(port, () => console.log('Server started on PORT : ' + port))
